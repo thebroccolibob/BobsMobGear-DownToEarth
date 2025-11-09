f0f370f4ffe10ff4bf829aaa788c551ce42427a0
thebroccolibob <thebroccolibob@gmail.com>
2025-11-09 05:05:48 -0800

    Added Leaf Fibers item

--- src/main/java/io/github/thebroccolibob/downtoearth/item/ModItems.java
+++ src/main/java/io/github/thebroccolibob/downtoearth/item/ModItems.java
@@ -1,17 +1,25 @@
 package io.github.thebroccolibob.downtoearth.item;
 
 import io.github.thebroccolibob.downtoearth.BobsMobGearDownToEarth;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item LEAF_FIBERS = registerItem("leaf_fibers", new Item(new Item.Settings()));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(BobsMobGearDownToEarth.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         BobsMobGearDownToEarth.LOGGER.info("Registering items for " + BobsMobGearDownToEarth.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(LEAF_FIBERS);
+        });
     }
 }
