b56b139d2edf9afc84e80705d9cbb8f9ac2cf0a7
thebroccolibob <thebroccolibob@gmail.com>
2025-11-10 16:37:04 -0800

    renamed mixin function for campfire siganls

--- src/main/java/io/github/thebroccolibob/downtoearth/mixin/CampfireBlockMixin.java
+++ src/main/java/io/github/thebroccolibob/downtoearth/mixin/CampfireBlockMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CampfireBlock.class)
 class CampfireBlockMixin {
     @Inject(method = "isSignalFireBaseBlock", at = @At("RETURN"), cancellable = true)
-    private void AltIsSignalFireBaseBlock(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+    private void AltSignalFireBaseBlock(BlockState state, CallbackInfoReturnable<Boolean> cir) {
         if (!cir.getReturnValue()) {
             if (state.isOf(ModBlocks.LEAF_FIBER_BALE)) {
                 cir.setReturnValue(true);
