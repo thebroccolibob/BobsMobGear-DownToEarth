94e813316a6ed12f3848704287c0c0f80f22213a
thebroccolibob <thebroccolibob@gmail.com>
2025-11-11 03:07:30 -0800

    Added ModTags class for future use

---
+++ src/main/java/io/github/thebroccolibob/downtoearth/util/ModTags.java
@@ -0,0 +1,22 @@
+package io.github.thebroccolibob.downtoearth.util;
+
+import io.github.thebroccolibob.downtoearth.BobsMobGearDownToEarth;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(BobsMobGearDownToEarth.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(BobsMobGearDownToEarth.MOD_ID, name));
+        }
+    }
+}
