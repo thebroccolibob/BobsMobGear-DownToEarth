e9901d31805e7d3e6ba29cfb5e3324d2f6a8cb5e
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-25 13:56:12 -0800

    Carving recipe B

--- src/main/java/io/github/thebroccolibob/downtoearth/emi/CarvingEmiRecipe.java
+++ src/main/java/io/github/thebroccolibob/downtoearth/emi/CarvingEmiRecipe.java
@@ -1,5 +1,6 @@
 package io.github.thebroccolibob.downtoearth.emi;
 
+import net.minecraft.item.BlockItem;
 import net.minecraft.util.Identifier;
 
 import dev.emi.emi.api.recipe.BasicEmiRecipe;
@@ -16,7 +17,7 @@ public class CarvingEmiRecipe extends BasicEmiRecipe {
     private static final EmiStack TOOL = EmiStack.of(ModItems.FLINT_TOOL.getDefaultStack());
 
     public CarvingEmiRecipe(Identifier id, EmiIngredient input, EmiStack output) {
-        super(DownToEarthEmiPlugin.CARVING, id, 76, 36);
+        super(DownToEarthEmiPlugin.CARVING, id, 76, 68);
         this.input = input;
         this.output = output;
         inputs.add(input);
@@ -26,10 +27,12 @@ public class CarvingEmiRecipe extends BasicEmiRecipe {
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
-        widgets.addTexture(EmiTexture.EMPTY_ARROW, 26, 19);
-        widgets.addSlot(input, 0, 18);
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, 26, 51);
+        if (input.getEmiStacks().getFirst().getItemStack().getItem() instanceof BlockItem blockItem)
+            widgets.add(new BlockWidget(blockItem.getBlock().getDefaultState(), 22, 18, 32));
+        widgets.addSlot(input, 0, 50);
         widgets.addSlot(TOOL, 29, 0)
                 .catalyst(true);
-        widgets.addSlot(output, 58, 18).recipeContext(this);
+        widgets.addSlot(output, 58, 50).recipeContext(this);
     }
 }
