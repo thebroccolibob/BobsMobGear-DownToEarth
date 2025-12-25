49a63dd6fc768fe1acb99cc24b9e9f6692838b7b
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-25 13:37:12 -0800

    Add tongs to hammering recipe

--- src/main/java/io/github/thebroccolibob/downtoearth/emi/HammeringEmiRecipe.java
+++ src/main/java/io/github/thebroccolibob/downtoearth/emi/HammeringEmiRecipe.java
@@ -7,11 +7,13 @@ import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
 import io.github.thebroccolibob.bobsmobgear.registry.BobsMobGearItemTags;
+import io.github.thebroccolibob.bobsmobgear.registry.BobsMobGearItems;
 
 public class HammeringEmiRecipe extends BasicEmiRecipe {
     private final EmiIngredient ingredient;
     private final EmiStack result;
     private final EmiIngredient hammer;
+    private static final EmiStack TONGS = EmiStack.of(BobsMobGearItems.SMITHING_TONGS);
 
     public HammeringEmiRecipe(Identifier id, EmiIngredient ingredient, EmiStack result) {
         super(DownToEarthEmiPlugin.HAMMERING, id, 92, 74);
@@ -20,13 +22,16 @@ public class HammeringEmiRecipe extends BasicEmiRecipe {
         inputs.add(ingredient);
         hammer = EmiIngredient.of(BobsMobGearItemTags.HAMMERS);
         catalysts.add(hammer);
+        catalysts.add(TONGS);
         outputs.add(result);
     }
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
         widgets.add(new HammeringWidget(ingredient.getEmiStacks().getFirst().getItemStack(), 22, 22, 48));
-        widgets.addSlot(ingredient, 4, 33);
+        widgets.addSlot(ingredient, 4, 28);
+        widgets.addSlot(TONGS, 4, 46)
+                .catalyst(true);
         widgets.addSlot(hammer, 37, 4)
                 .catalyst(true);
         widgets.addSlot(result, 70, 33)
